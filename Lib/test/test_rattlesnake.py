28ac7226dc0478443321b08f03fe32be7c10eecd
Skip Montanaro <skip.montanaro@gmail.com>
2020-03-27 08:37:25 -0700

    start on some formal unit tests

---
+++ Lib/test/test_rattlesnake.py
@@ -0,0 +1,19 @@
+import unittest
+from test import support
+
+from rattlesnake import instructions, opcodes
+
+class InstructionTest(unittest.TestCase):
+    def test_nop(self):
+        nop = instructions.NOPInstruction(opcodes.ISET.opmap['NOP'])
+        self.assertEqual(nop.opargs, (0,))
+
+    def test_src_dst(self):
+        lfr = opcodes.ISET.opmap['LOAD_FAST_REG']
+        load = instructions.LoadFastInstruction(lfr, (1, 2))
+        self.assertEqual(load.first(), ())
+        self.assertEqual(load.dest_registers(), (1,))
+        self.assertEqual(load.source_registers(), (2,))
+        self.assertEqual(load.rest(), ())
+        load.update_opargs(source=(3,))
+        self.assertEqual(load.opargs, (1, 3))
