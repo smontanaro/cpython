28f66abc496786fcc7511a836aa8df6c653a2ede
Skip Montanaro <skip.montanaro@gmail.com>
2020-03-28 06:28:36 -0700

    a couple debug functions

--- Lib/rattlesnake/util.py
+++ Lib/rattlesnake/util.py
@@ -7,3 +7,18 @@ def enumerate_reversed(seq):
     for obj in reversed(seq):
         n -= 1
         yield (n, obj)
+
+def decode_oparg(oparg):
+    "split oparg into four bytes - debug helper"
+    arg4 = oparg >> 24
+    arg3 = oparg >> 16 & 0xff
+    arg2 = oparg >> 8 & 0xff
+    arg1 = oparg & 0xff
+    return (arg4, arg3, arg2, arg1)
+
+def encode_oparg(tup):
+    "smash tuple into oparg - debug helper"
+    oparg = 0
+    for elt in tup:
+        oparg = oparg << 8 | elt
+    return oparg
