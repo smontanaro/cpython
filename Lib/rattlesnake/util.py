9b3dff9c36bbdbb1e078009303829af4ee1310d1
Skip Montanaro <skip.montanaro@gmail.com>
2020-03-31 06:46:24 -0700

    +LineNumberDict, decode_oparg tweak & some test cases (some broken)

--- Lib/rattlesnake/util.py
+++ Lib/rattlesnake/util.py
@@ -1,5 +1,7 @@
 "miscellaneous"
 
+import dis as _dis
+
 def enumerate_reversed(seq):
     "Enumerate a sequence in reverse. Thank you Chris Angelico."
     # https://code.activestate.com/lists/python-list/706210/
@@ -8,17 +10,73 @@ def enumerate_reversed(seq):
         n -= 1
         yield (n, obj)
 
-def decode_oparg(oparg):
-    "split oparg into four bytes - debug helper"
-    arg4 = oparg >> 24
-    arg3 = oparg >> 16 & 0xff
-    arg2 = oparg >> 8 & 0xff
-    arg1 = oparg & 0xff
-    return (arg4, arg3, arg2, arg1)
+def decode_oparg(oparg, minimize=True):
+    """Split oparg into (up to) four bytes.
+
+    If minimize is True (default), leading zeros will be trimmed from
+    the tuple.
+
+    """
+    args = [
+        oparg >> 24,
+        oparg >> 16 & 0xff,
+        oparg >> 8 & 0xff,
+        oparg & 0xff,
+    ]
+    if minimize:
+        while args[0] == 0:
+            del args[0]
+    return tuple(args)
 
 def encode_oparg(tup):
-    "smash tuple into oparg - debug helper"
+    "smash tuple back into oparg int"
     oparg = 0
     for elt in tup:
         oparg = oparg << 8 | elt
     return oparg
+
+class LineNumberDict:
+    """Return line number associated with max key <= user's key.
+
+    For example, if we have:
+
+    fdict = LineNumberDict(func.__code__)
+
+    where func.__code__'s lnotab looks like this dict:
+
+    {0: 4, 6: 5, 16: 6, 26: 7}
+
+    fdict[16] returns 6, but fdict[15] returns 5. Any key greater than
+    26 would return 7.  Any key less than the smallest key would raise
+    KeyError.  If a max key value is set, e.g.:
+
+    fdict = LineNumberDict(func.__code__, maxkey=36)
+
+    any key greater than that would also raise KeyError.
+
+    """
+
+    def __init__(self, codeobj=None, maxkey=None):
+        self.src_dict = {}
+        for (byt, lno) in _dis.findlinestarts(codeobj):
+            self.src_dict[byt] = lno
+        self.maxkey = maxkey
+        self.minkey = min(self.src_dict)
+        self.true_maxkey = max(self.src_dict)
+
+    def __getitem__(self, key):
+        if (key < self.minkey or
+            self.maxkey is not None and key > self.maxkey):
+            raise KeyError(f"Key {key} out of range.")
+
+        if key > self.true_maxkey:
+            return self.src_dict[self.true_maxkey]
+
+        while True:
+            val = self.src_dict.get(key, None)
+            if val is not None:
+                return val
+            key -= 1
+
+    def __getattr__(self, attr):
+        return getattr(self.src_dict, attr)
--- Lib/test/test_rattlesnake.py
+++ Lib/test/test_rattlesnake.py
@@ -2,7 +2,8 @@ import unittest
 from test import support
 
 from rattlesnake.converter import InstructionSetConverter
-from rattlesnake import instructions, opcodes
+from rattlesnake import instructions, opcodes, util
+
 
 class InstructionTest(unittest.TestCase):
     def test_nop(self):
@@ -83,6 +84,39 @@ class InstructionTest(unittest.TestCase):
         #                               b'\x84\x04l\x12\x85\x02\x7f\x02'
         #                               b'l\x03\x80\x01l\x01l\x03z\x00\x7f\x02'))
 
+    def test_long_block_function(self):
+        isc = InstructionSetConverter(_long_block.__code__)
+        isc.find_blocks()
+        isc.convert_address_to_block()
+        isc.gen_rvm()
+        isc.forward_propagate_fast_loads()
+        isc.backward_propagate_fast_stores()
+        isc.delete_nops()
+        opcodes = _get_opcodes(isc.rvm_blocks)
+        isc.display_blocks(isc.rvm_blocks)
+        self.assertEqual(opcodes[0][:3], [132, 108, 133])
+
+    def test_util_decode(self):
+        self.assertEqual(util.decode_oparg(71682), (1, 24, 2))
+        self.assertEqual(util.decode_oparg(71682, False), (0, 1, 24, 2))
+
+    def test_util_encode(self):
+        self.assertEqual(util.encode_oparg((1, 24, 2)), 71682)
+
+    def test_util_LineNumberDict(self):
+        lno_dict = util.LineNumberDict(_get_opcodes.__code__)
+        first_lineno = _get_opcodes.__code__.co_firstlineno
+        self.assertEqual(lno_dict[26], 4 + first_lineno)
+        self.assertEqual(lno_dict[0], 1 + first_lineno)
+        self.assertEqual(lno_dict[80], 6 + first_lineno)
+        with self.assertRaises(KeyError):
+            lno_dict[-10]
+
+        lno_dict = util.LineNumberDict(_get_opcodes.__code__, maxkey=75)
+        with self.assertRaises(KeyError):
+            lno_dict[80]
+
+
 def _branch_func(a):
     if a > 4:
         return a
@@ -99,3 +133,41 @@ def _get_opcodes(blocks):
         for inst in block:
             ops[-1].append(inst.opcode)
     return ops
+
+_A_GLOBAL = 42
+def _long_block(s, b):
+    if s > b:
+        s = b - 21
+        b = s * 44
+        s = b + 4
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        b = s * 44
+        s = b + 4
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        b = s * 44
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        b = s + 24
+        s = b - 21
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        b = s - 21
+        s = b + _A_GLOBAL
+        b = s + 24
+        s = b - 21
+        return s
+    return b - 1
