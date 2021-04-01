fc999e390316c28886d11b954cdf7d77ac7f3558
Skip Montanaro <skip.montanaro@gmail.com>
2021-04-01 09:59:16 -0500

    a slightly more involved function, waiting on FORMAT_VALUE_REG

---
+++ Lib/test/test_rvm/test_alg.py
@@ -0,0 +1,19 @@
+"""Ever-so-slightly bigger tests, starting with some simple algorithms."""
+
+import unittest
+
+from . import InstructionTest
+
+class AlgTest(InstructionTest):
+    @unittest.skip('FORMAT_VALUE_REG not yet implemented')
+    def test_fib(self):
+        def fib(n):
+            if n < 0:
+                raise ValueError(f"Fibonacci sequence does not support negative numbers ({n})")
+            if n < 2:
+                return 1
+            return fib(n-1) + fib(n-2)
+        (pyvm, rvm) = self.function_helper(fib)
+        with self.assertRaises(ValueError):
+            rvm(-99)
+        self.assertEqual(pyvm(4), rvm(4))
