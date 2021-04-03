15b1b7063a3356bd2650c231ae9eb6fd9a186fe2
Skip Montanaro <skip.montanaro@gmail.com>
2021-04-03 18:37:16 -0500

    forgot this for FORMAT_VALUE_REG & BUILD_STRING

---
+++ Lib/test/test_rvm/test_string.py
@@ -0,0 +1,17 @@
+
+import unittest
+
+from . import InstructionTest
+
+class StringTest(InstructionTest):
+    def test_build_string(self):
+        def build_string():
+            return f""
+        (pyvm, rvm) = self.function_helper(build_string)
+        self.assertEqual(pyvm(), rvm())
+
+    def test_format_value(self):
+        def format_value(a, b):
+            return f"{a} 4 {b:.2f} {(a+b)!r}"
+        (pyvm, rvm) = self.function_helper(format_value)
+        self.assertEqual(pyvm(14, -3), rvm(14, -3))
