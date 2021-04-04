c4ec7e97c3578cf292ad18d81c104dfc7f46827f
Skip Montanaro <skip.montanaro@gmail.com>
2021-04-04 12:15:40 -0500

    skip this for a different reason

--- Lib/test/test_rvm/test_alg.py
+++ Lib/test/test_rvm/test_alg.py
@@ -5,7 +5,17 @@ import unittest
 from . import InstructionTest
 
 class AlgTest(InstructionTest):
-    @unittest.skip('LOAD_DEREF_REG not yet implemented.')
+    @unittest.skip('Something not right regarding free vrbls.')
+    # ERROR: test_fib (test.test_rvm.test_alg.AlgTest)
+    # ----------------------------------------------------------------------
+    # Traceback (most recent call last):
+    #   File "/home/skip/src/python/rvm/Lib/test/test_rvm/test_alg.py", line 16, in test_fib
+    #     (pyvm, rvm) = self.function_helper(fib)
+    #   File "/home/skip/src/python/rvm/Lib/test/test_rvm/__init__.py", line 42, in function_helper
+    #     rvm_replace_code(rvm, pyvm_code, isc)
+    #   File "/home/skip/src/python/rvm/Lib/test/test_rvm/__init__.py", line 115, in rvm_replace_code
+    #     func.__code__ = rvm_code
+    # ValueError: rvm() requires a code object with 0 free vars, not 1
     def test_fib(self):
         def fib(n):
             if n < 0:
